5d9443d6940ffc0cd385fb155d97bcd599605c5a
vedoctor <mico.mg52@gmail.com>
2021-05-22 09:59:23 +0200

    minor bugfix

--- map_generator.py
+++ map_generator.py
@@ -1,8 +1,8 @@
 # made by vedoctor on may 16th, 2021
 
 # jak wywolujesz funkcje to jako argument string nazwa obrazka, koniecznie jpg
-# obrazek musi byc idealnie kwadratowy
-# czarny piksel to 1, bialy to 0
+# bialy pixel to 0, czarny to 1, czerwony to 2
+# najlepiej kolorki: biały (255, 255, 255), czarny (0, 0, 0), czerwony (255, 0, 0)
 
 from PIL import Image  # biblioteka - najpierw musisz zrobic w terminalu pip install Pillow
 # btw fajna biblioteka do processingu obrazow
@@ -21,8 +21,8 @@ def generate_map_array(img_name):
             color = im.getpixel((pixel, row))  # kolor pixela o adresie (pixel, row)
             if color < (20, 20, 20):  # jesli kolor pixela jest mniejszy niz 20, 20, 20 (czyli pixel jest ciemny)
                 row_array.append(1)  # to zapisz do tymczasowej tablicy jeden
-            if color[0] > 200 and color[1, 2] < (20, 20):  # jesli kolor pixela R jest wiekszy niz 200 i jednoczesnie
-                # G i B sa mniejsze od 20 (czyli bardzo czerwony)
+            elif color[0] > 200 and color[1] < 20 and color[2] < 20:  # jesli kolor pixela R jest wiekszy niz 200 i
+                # jednoczesnie G i B sa mniejsze od 20 (czyli bardzo czerwony)
                 row_array.append(2)  # to zapisz do tymczasowej tablicy dwa
             else:  # w kazdym innym przypadku (czyli pixel jasny)
                 row_array.append(0)  # zapisz zero
@@ -32,7 +32,7 @@ def generate_map_array(img_name):
 
 
 # ponizej tylko wypisywanie, mozesz pominac ofc
-for row in generate_map_array('map.jpg'):
+for row in generate_map_array('map2.jpg'):
     print(row)
 
 # mam nadzieje ze wszystko zrozumiale
